d06bc73fb15e87ec67b34908ce304bf2377274a6
Aavtic <89965681+Aavtic@users.noreply.github.com>
2023-08-26 18:41:56 +0530

    Delete src/bin directory

--- src/bin/test1.rs
+++
@@ -1,5 +0,0 @@
-use magic_crypt::{new_magic_crypt, MagicCryptTrait};
-use serde::de::IntoDeserializer;
-fn main() {
-    let password = String::from_utf8(vec![104, 101, 108, 108, 111, 102, 114, 105, 101, 110, 100]).unwrap();
-}
